49ac08c9177da366602780962dc0c17d8334e71f
ibrargrayphite <ibrar.grayphite@gmail.com>
2026-02-17 16:26:50 +0500

    refactor: Simplify CORS configuration by always allowing all origins.

--- config/settings.py
+++ config/settings.py
@@ -149,30 +149,31 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-# CORS settings
-CORS_ALLOWED_ORIGINS = ["*"]
-CORS_ALLOW_ALL_ORIGINS = DEBUG # Use allow all only if debug is true if you want, but better to use allowed origins
-CSRF_TRUSTED_ORIGINS = ["*"]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-CORS_ALLOW_HEADERS = [
-    "Authorization",
-    "Content-Type",
-    "X-Requested-With",
-    "Accept",
-    "Origin",
-    "User-Agent",
-    "X-CSRFTOKEN",
-    "Access-Control-Allow-Origin",
-    "Access-Control-Allow-Headers",
-]
-CORS_ALLOW_CREDENTIALS = True
+# # CORS settings
+# CORS_ALLOWED_ORIGINS = ["*"]
+# CORS_ALLOW_ALL_ORIGINS = DEBUG # Use allow all only if debug is true if you want, but better to use allowed origins
+# CSRF_TRUSTED_ORIGINS = ["*"]
+# CORS_ALLOW_METHODS = [
+#     "GET",
+#     "POST",
+#     "PUT",
+#     "PATCH",
+#     "DELETE",
+#     "OPTIONS",
+# ]
+# CORS_ALLOW_HEADERS = [
+#     "Authorization",
+#     "Content-Type",
+#     "X-Requested-With",
+#     "Accept",
+#     "Origin",
+#     "User-Agent",
+#     "X-CSRFTOKEN",
+#     "Access-Control-Allow-Origin",
+#     "Access-Control-Allow-Headers",
+# ]
+# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # drf-spectacular settings
 SPECTACULAR_SETTINGS = {
