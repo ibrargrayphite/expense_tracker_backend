e3498e27012721eb92cf019b15717fad01e5feaa
ibrargrayphite <ibrar.grayphite@gmail.com>
2026-02-23 00:37:05 +0500

    Change EMAIL_PORT from 587 to 465.

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'apikey'
